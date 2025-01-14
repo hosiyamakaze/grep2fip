d6bb9347f1e51567d9b95524252153bbef18c3f8
hosiyamakaze <64181800+hosiyamakaze@users.noreply.github.com>
2025-01-14 18:44:52 +0900

    Swap columns: 1st 'SideIndicator', 2nd 'InputObject'(line no added)

--- diff.ps1
+++ diff.ps1
@@ -1,4 +1,4 @@
-#SJIS $Workfile: diff.ps1 $$Revision: 2 $$Date: 25/01/12 15:03 $
+#SJIS $Workfile: diff.ps1 $$Revision: 4 $$Date: 25/01/12 19:00 $
 #$NoKeywords: $
 
 param (
@@ -9,6 +9,13 @@ param (
 if(-not $OriginalFile){$OriginalFile = Read-Host "OriginalFile"}
 if(-not $TargetFile){$TargetFile = Read-Host "TargetFile"}
 
-$difdata = Compare-Object -IncludeEqual -ReferenceObject (Get-Content $OriginalFile)  -DifferenceObject (Get-Content $TargetFile)
-Write-Host "SideIndicator","InputObject" -Separator `t
-$difdata | ForEach-Object { Write-Host $_.SideIndicator, $_.InputObject -Separator `t}
+$refdata=(Get-Content $OriginalFile)
+$difdata=(Get-Content $TargetFile)
+$diff = Compare-Object  -ReferenceObject $refdata  -DifferenceObject $difdata
+Write-Host "Ref","Dif","SideIndicator","InputObject" -Separator `t
+$diff | ForEach-Object {
+    $file = if ($_.SideIndicator -eq '<=') { $refdata } else { $difdata }
+    $lineref = $refdata.IndexOf($_.InputObject) + 1
+    $linedif = $difdata.IndexOf($_.InputObject) + 1
+    Write-Host $lineref,$linedif,$($_.SideIndicator),$($_.InputObject) -Separator `t
+}
